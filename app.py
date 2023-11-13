5fc48c5d9aeb96e8f42a720a9f92cd0159d78d64
Elkin <ersantis@uninorte.edu.co>
2023-11-12 20:10:48 -0500

    add subjects route

--- app.py
+++ app.py
@@ -3,9 +3,8 @@ import config
 import	mysql.connector
 
 app = Flask(__name__)
+app.secret_key = "super secret key"
 
-
-#Connect to Database
 try:
     mydb = mysql.connector.connect(
     host=config.MYSQL_ADDON_HOST,
@@ -16,14 +15,57 @@ try:
     print('Succesfull')
 except Exception as e:
     print(e)
-
-
-#Pagina principal - Login
-@app.route('/')
-def index():
-    return render_template('index.html')
     
+    
+#Pagina principal - Login
+@app.route('/',methods=['GET'])
+def home():
+    return render_template('index2.html')
 
+
+@app.route('/login', methods=['POST'])
+def login():
+    id_student = request.form['id_student']
+    password = request.form['password']
+
+    cur = mydb.cursor()
+    cur.execute("SELECT * FROM Students WHERE idStudent = %s AND Password = %s", (id_student, password))
+    user = cur.fetchone()
+    print(user)
+    cur.close()
+
+    if user is not None:
+        session['idStudent'] = id_student
+        session['Name'] = user[2]
+        session['LastName'] = user[4]
+        session['Email'] = user[5]
+        print('Bienvenido')
+        return redirect(url_for('subjects'))
+    else:
+        return render_template('index.html', message="Las credenciales no son correctas")    
+
+@app.route('/subjects', methods=['GET'])
+def subjects():
+    StudentID = session['idStudent']
+    cur = mydb.cursor()
+    cur.execute("SELECT s.* FROM Subject s join SubjectsByStudent sbs on s.idSubject = sbs.SubjectID WHERE StudentID = %s", [StudentID])
+    sub = cur.fetchall()
+    print(cur.description)
+
+
+    insertObject = []
+    columnNames = [column[0] for column in cur.description]
+    for record in sub:
+        insertObject.append(dict(zip(columnNames, record)))
+    cur.close()
+    print(insertObject)
+
+    return render_template('subjects.html', tasks = insertObject)
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
         app.run(debug=True)
