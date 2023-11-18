d1f08061a22c0fef08aea2305998e80e526b247e
Elkin <ersantis@uninorte.edu.co>
2023-11-18 17:34:27 -0500

    add js script

---
+++ static/js/subjects.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Obtener todas las cartas y agregar el evento de clic a cada una
+    var cards = document.querySelectorAll('.card');
+    console.log(cards)
+
+    cards.forEach(function(card) {
+        // Verificar si el ID cumple con ciertos criterios (por ejemplo, si contiene '_dynamic')
+        if (card.id.includes('miDiv')) {
+            card.addEventListener('click', function() {
+                // Acción a realizar cuando se hace clic en una carta
+                console.log(card);
+                var cardName = card.id.split('_')[1];
+                var smallContent = card.querySelector('.nrc').innerText;
+
+                // Redirigir a la página correspondiente
+                window.location.href = '/subjects/' + cardName + '/' + smallContent;
+            });
+        }
+    });
+});
