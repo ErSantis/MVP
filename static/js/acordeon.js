f118be547f9229240589e38f6edc338a3bb65af2
Elkin <ersantis@uninorte.edu.co>
2023-11-23 00:33:46 -0500

    Add collapsible functionality to acordeon.js

---
+++ static/js/acordeon.js
@@ -0,0 +1,20 @@
+const collapsibleButtons = document.querySelectorAll(
+    ".collapsible-trigger-btn"
+);
+
+collapsibleButtons.forEach((collapsibleButton) => {
+    const collapsibleContentDataHeight =
+        collapsibleButton.nextElementSibling.offsetHeight;
+    collapsibleButton.nextElementSibling.style.height = 0;
+    collapsibleButton.addEventListener("click", (e) => {
+        if (
+            !e.currentTarget.parentElement.classList.contains("collapsible-tab__open")
+        ) {
+            e.currentTarget.parentElement.classList.toggle("collapsible-tab__open");
+            e.currentTarget.nextElementSibling.style.height = `${collapsibleContentDataHeight}px`;
+        } else {
+            e.currentTarget.parentElement.classList.remove("collapsible-tab__open");
+            e.currentTarget.nextElementSibling.style.height = 0;
+        }
+    });
+});
