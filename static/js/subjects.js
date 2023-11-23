666ac5c2fcfeda204d0da1f656ebefcad2c64513
Elkin <ersantis@uninorte.edu.co>
2023-11-23 00:34:13 -0500

    Refactor click event listener in subjects.js

--- static/js/subjects.js
+++ static/js/subjects.js
@@ -1,20 +1,19 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     // Obtener todas las cartas y agregar el evento de clic a cada una
     var cards = document.querySelectorAll('.card');
-    console.log(cards)
 
-    cards.forEach(function(card) {
+    cards.forEach(function (card) {
         // Verificar si el ID cumple con ciertos criterios (por ejemplo, si contiene '_dynamic')
-        if (card.id.includes('miDiv')) {
-            card.addEventListener('click', function() {
-                // Acción a realizar cuando se hace clic en una carta
-                console.log(card);
-                var cardName = card.id.split('_')[1];
-                var smallContent = card.querySelector('.nrc').innerText;
 
-                // Redirigir a la página correspondiente
-                window.location.href = '/subjects/' + cardName + '/' + smallContent;
-            });
-        }
+        card.addEventListener('click', function () {
+            // Acción a realizar cuando se hace clic en una carta
+            console.log(card);
+            var cardName = card.id.split('_')[1];
+            var smallContent = card.querySelector('.nrc').innerText;
+
+            // Redirigir a la página correspondiente
+            window.location.href = '/subjects/' + cardName + '/' + smallContent;
+        });
+
     });
 });
