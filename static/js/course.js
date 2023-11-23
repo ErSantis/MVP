574a3d59ea7cf024febb6e8d7b64048a713f95d0
Elkin <ersantis@uninorte.edu.co>
2023-11-23 00:33:59 -0500

    Add sticky navigation functionality to course page

---
+++ static/js/course.js
@@ -0,0 +1,90 @@
+document.addEventListener("DOMContentLoaded", function () {
+	class StickyNavigation {
+		constructor() {
+			this.currentId = null;
+			this.currentTab = null;
+			this.tabContainerHeight = 70;
+			let self = this;
+			$('.et-hero-tab').click(function (event) {
+				self.onTabClick(event, $(this));
+			});
+			$(window).scroll(() => { this.onScroll(); });
+			$(window).resize(() => { this.onResize(); });
+		}
+
+		onTabClick(event, element) {
+			event.preventDefault();
+			let targetId = element.attr('href');
+			let $targetSection = $(targetId);
+
+			if ($targetSection.length > 0) {
+				// Cerrar todos los acordeones
+				$('.collapsibles-wrapper').removeClass('collapsible-tab__open');
+				$('.collapsible-content').css('height', 0);
+
+				// Abrir el acordeón en la sección específica
+				let $collapsibleWrapper = $targetSection.find('.collapsibles-wrapper');
+				$collapsibleWrapper.addClass('collapsible-tab__open');
+				$collapsibleWrapper.find('.collapsible-content').css('height', 'auto');
+
+				// Desplazar a la sección
+				let scrollTop = $targetSection.offset().top - $('.et-hero-tabs-container').outerHeight(true) + 1;
+				$('html, body').animate({ scrollTop: scrollTop }, 600);
+			}
+		}
+
+		onScroll() {
+			this.checkTabContainerPosition();
+			this.findCurrentTabSelector();
+		}
+
+		onResize() {
+			if (this.currentId) {
+				this.setSliderCss();
+			}
+		}
+
+		checkTabContainerPosition() {
+			let offset = $('.et-hero-tabs').offset().top + $('.et-hero-tabs').height() - this.tabContainerHeight;
+			if ($(window).scrollTop() > offset) {
+				$('.et-hero-tabs-container').addClass('et-hero-tabs-container--top');
+			} else {
+				$('.et-hero-tabs-container').removeClass('et-hero-tabs-container--top');
+			}
+		}
+
+		findCurrentTabSelector(element) {
+			let newCurrentId;
+			let newCurrentTab;
+			let self = this;
+			$('.et-hero-tab').each(function () {
+				let id = $(this).attr('href');
+				let offsetTop = $(id).offset().top - self.tabContainerHeight;
+				let offsetBottom = $(id).offset().top + $(id).height() - self.tabContainerHeight;
+				if ($(window).scrollTop() > offsetTop && $(window).scrollTop() < offsetBottom) {
+					newCurrentId = id;
+					newCurrentTab = $(this);
+				}
+			});
+			if (this.currentId != newCurrentId || this.currentId === null) {
+				this.currentId = newCurrentId;
+				this.currentTab = newCurrentTab;
+				this.setSliderCss();
+			}
+		}
+
+		setSliderCss() {
+			let width = 0;
+			let left = 0;
+			if (this.currentTab) {
+				width = this.currentTab.css('width');
+				left = this.currentTab.offset().left;
+			}
+			$('.et-hero-tab-slider').css('width', width);
+			$('.et-hero-tab-slider').css('left', left);
+		}
+	}
+
+	new StickyNavigation();
+	
+});
